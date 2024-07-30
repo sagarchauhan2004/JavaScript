76e46ccecf7ef411a5976569cc9af3d92ccae916
SAGAR CHAUHAN <151016944+sagarchauhan2004@users.noreply.github.com>
2024-07-30 04:48:28 +0000

    Dates commit

---
+++ 01_basic/07_dates.js
@@ -0,0 +1,29 @@
+// let myDates = new Date()
+
+// console.log(myDates);
+
+// console.log(myDates.toString());
+
+// console.log(myDates.toDateString());
+
+// console.log(myDates.toLocaleString());
+
+// console.log(typeof (myDates));
+
+// let myCreatedDate = new Date(2023 , 3 , 23)
+
+// let myCreatedDate = new Date(2023 , 0 , 23 , 5 , 3)
+
+// let myCreatedDate = new Date("2023-01-14")
+
+let myCreatedDate = new Date("01-14-2023");
+
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+
+// console.log(myCreatedDate.getTime());
+
+console.log(Math.floor(Date.now() / 1000));
