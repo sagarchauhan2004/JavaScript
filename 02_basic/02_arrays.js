14a12b70f2e014241a7788c29963910468ad12dc
SAGAR CHAUHAN <151016944+sagarchauhan2004@users.noreply.github.com>
2024-07-30 12:51:23 +0000

    arrays commit

---
+++ 02_basic/02_arrays.js
@@ -0,0 +1,27 @@
+const marval_heros = ["thor", "ironman", "spiderman"];
+
+const dc_heros = ["superman", "flash", "batman"];
+
+// marval_heros.push(dc_heros)
+
+// console.log(marval_heros);
+
+// console.log(marval_heros[3][1]);
+
+// const newArr = marval_heros.concat(dc_heros)
+
+// console.log(newArr);
+
+// const all_new_heros = [...marval_heros,...dc_heros]
+
+// console.log(all_new_heros);
+
+// const another_array = [1 , 2 , 3 , [ 4 , 5 , 6 ] , 7 , [ 6 , 7 [ 4 , 5 ]]]
+
+// const real_Array = another_array.flat(Infinity);
+
+// console.log(real_Array);
+
+console.log(Array.isArray("sagar"));
+
+console.log(Array.from("sagar"));
