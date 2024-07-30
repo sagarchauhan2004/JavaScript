8d966729eb43ab478e9efa897860769ca98ab46a
SAGAR CHAUHAN <151016944+sagarchauhan2004@users.noreply.github.com>
2024-07-30 05:18:35 +0000

    arrays commit

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,51 @@
+const myArr = [0, 1, 2, 3, 4, 5];
+
+const myHeros = ["shaktiman", "naagraj", "krish"];
+
+const myArr2 = new Array(1, 2, 3, 4);
+
+// console.log(myArr);
+
+// myArr.push(6)
+// console.log(myArr);
+
+// myArr.pop(2);
+// console.log(myArr);
+
+// myArr.push(2)
+// console.log(myArr);
+
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(9);
+// console.log(myArr);
+
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.includes(4));
+// console.log(myArr);
+
+// console.log(myArr.indexOf(19));
+// console.log(myArr);
+
+// console.log(myArr.indexOf(3));
+// console.log(myArr);
+
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+console.log("A " , myArr);
+
+const myn1 = myArr.slice(1 , 3)
+
+console.log(myn1);
+console.log("B " , myArr);
+
+const myn2 = myArr.splice(1 , 3);
+console.log(myn2);
+console.log("c ", myArr);
