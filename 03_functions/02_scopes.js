c5d52103684a700ae17a5d0b9163283ba853ef43
SAGAR CHAUHAN <151016944+sagarchauhan2004@users.noreply.github.com>
2024-07-30 15:45:15 +0000

    scopes commit

--- 03_functions/02_scopes.js
+++ 03_functions/02_scopes.js
@@ -1,12 +1,24 @@
-let a = 300;
+// let a = 300;
 
-if (true) {
-  let a = 10;
-  const b = 20;
-  var c = 30;
-  console.log("INNER :", a);
+// if (true) {
+//   let a = 10;
+//   const b = 20;
+//   var c = 30;
+//   console.log("INNER :", a);
+// }
+
+// console.log("OUTER :",a);
+// // console.log(b);
+// console.log(c);
+
+function one() {
+  const username = "sagar";
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  // console.log(website);
+  two();
 }
 
-console.log("OUTER :",a);
-// console.log(b);
-console.log(c);
+one();
