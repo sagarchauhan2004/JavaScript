cc0bf81b936e79b11ec5ff984a9799311d0998a9
SAGAR CHAUHAN <151016944+sagarchauhan2004@users.noreply.github.com>
2024-07-30 18:04:26 +0000

    iife commit

---
+++ 03_functions/04_iife.js
@@ -0,0 +1,9 @@
+// Immediately Invoking Function Expressions (IIFE)
+
+(function chai() {
+  console.log("DB CONNECTED");
+})();
+
+((name) => {
+  console.log(`DB CONNECTED TWO ${name}`);
+})("sagar");
